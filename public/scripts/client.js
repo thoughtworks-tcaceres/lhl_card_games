b4b7615fe29085b01d87dfffbd3b31bd897119b7
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-08-01 01:46:49 +0000

    made the game direction dynamic

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,5 @@
-let socket = io.connect('https://lhl-card-games.herokuapp.com/');
-// let socket = io.connect('http://localhost:1000/');
+// let socket = io.connect('https://lhl-card-games.herokuapp.com/');
+let socket = io.connect('http://localhost:8080/');
 
 // Handle socket response
 
@@ -9,7 +9,10 @@ socket.on('directToGame', (data) => {
   // console.log(data)
   // window.location.href = `/enterGame/${data.uniqueRoomName}`;
   $('#lobby').slideUp(100);
-  $('#showGame').toggle(1000);
+
+  console.log('MAKI CHAN')
+  console.log(`#gameFor${data.uniqueRoomName}`)
+  $(`#gameFor${data.gameId}`).toggle(1000);
 });
 
 // --------------------------
